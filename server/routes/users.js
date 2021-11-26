4a6deb75810ba68473d053268d3d7db39d412d95
tehyaarassman <tehyaarassman@gmail.com>
2021-11-26 12:25:42 -0600

    test add task

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,22 +8,31 @@ const bcrypt = require('bcrypt');
 // requests to '/users'
 
 //addRewards - routes to the usersController.addReward method, the method points back here after completion and confirmation is sent back to client
-router.post('/', usersController.addUser, (req, res) => {
+router.post('/', usersController.addUser,(req, res) => {
   //bcrypt stuff
-  res.status(200).json({ message: 'User added ' });
+  const token = jwt.sign({ user: res.locals.email }, secret);
+  console.log(token);
+  res.cookie("access_token", token, {
+    httpOnly: true,
+  })
+  .status(200)
+  .json({ message: "Signed up successfully"});
+
+  // res.status(200).json({ message: 'User added ' });
   // or whatever res.locals we decide to store our data in
 });
 
 
-router.post('/login', usersController.getUser, async (req, res) => {
+router.post('/login', usersController.getUser, (req, res) => {
   //bcrypt stuff
   // console.log('inside post')
-  const comparePass = await bcrypt.compare(req.body.password, res.locals.password[0].password)
+  console.log(res.locals.password[0].password);
+  const comparePass = bcrypt.compareSync(req.body.password, res.locals.password[0].password)
+  // console.log(compare)
   if (comparePass) {
     // console.log('INSIDE IF')
     const token = jwt.sign({ user: res.locals.email }, secret);
-    console.log(token);
-    return res.cookie("access_token", token, {
+    res.cookie("access_token", token, {
       httpOnly: true,
     })
     .status(200)
@@ -33,6 +42,9 @@ router.post('/login', usersController.getUser, async (req, res) => {
     res.json('Wrong password. Try again.');
   }
 });
+
+//When logout logic: delete the cookie
+
 //getRewards - routes to the usersController.getAllRewards method, the returned response from the method is jsonified and sent to the client
 // router.get('/', usersController.getAllRewards, (req, res, next) => {
 //   res.status(200).json(res.locals.rewardsList)
